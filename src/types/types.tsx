2347b4e96a751d9b0347b27dc7e228d142c48ee5
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-23 21:24:22 +0500

    added Itodo interface

---
+++ src/types/types.tsx
@@ -0,0 +1,8 @@
+export interface ITodo {
+    "activity":string,
+    "accessibility":number,
+    "type":"cooking" | "education" | "recreational" | "social" | "diy" | "charity" | "relaxation" | "music" | "busywork",
+    "participants": number,
+    "price":number,
+    "key":number
+}

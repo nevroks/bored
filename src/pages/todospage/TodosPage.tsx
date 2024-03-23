211d830985bbcbac7408565cec310f192dc310c2
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-23 22:04:51 +0500

    added layout of pages and styles to them

---
+++ src/pages/todospage/TodosPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from "./style.module.css";
+
+const TodosPage = () => {
+    return (
+        <div className={classes.page__content}>
+
+        </div>
+    );
+};
+
+export default TodosPage;

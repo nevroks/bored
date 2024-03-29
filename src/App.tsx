30c51364850087f7ea5d5c900e1de7bd0d3026ae
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-29 19:34:52 +0500

    added login page to routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Layout from "./components/layout/Layout.tsx";
 import HomePage from "./pages/homepage/HomePage.tsx";
 import TodosPage from "./pages/todospage/TodosPage.tsx";
 import ProfilePage from "./pages/profilepage/ProfilePage.tsx";
+import LoginPage from "./pages/loginpage/LoginPage.tsx";
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
   return (
     <>
       <Routes>
-        {/*<Route path={"/login"} element={<LoginPage/>}/>*/}
+        <Route path={"/login"} element={<LoginPage/>}/>
         <Route path="/" element={<Layout/>}>
           <Route index element={<HomePage/>}/>
           <Route path="todos" element={<TodosPage/>}/>

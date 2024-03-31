d430d68db878a5dfca4dfa6a603fcfd16cb82685
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-31 23:25:18 +0500

    filled and styled header

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import classes from "./style.module.css";
+import {NavLink} from "react-router-dom";
 
 const Header = () => {
     return (
             <header className={classes.header}>
-
+                <h2>Logo</h2>
+                <nav className={classes.header__navigation}>
+                    <NavLink to={"/"}>Страница подбора</NavLink>
+                    <NavLink to={"/profile"}>Профиль</NavLink>
+                </nav>
             </header>
     );
 };

77cbbe92fe5bbdcabfd314b4d68fde863d19fba7
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-31 23:27:56 +0500

    used popup with filter and added logic of redirecting if not logined

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 import classes from "./style.module.css";
+import {json, useNavigate} from "react-router-dom";
+import {useAppDispatch, useAppSelector} from "../../hooks/reduxHooks.tsx";
+import Button from "../../components/ui/button/Button.tsx";
+import {setAuth} from "../../store/auth/authSlice.ts";
+import UseGetData from "../../hooks/useGetData.ts";
+import Todo from "../../components/todos/Todo.tsx";
+import Filter from "../../components/filter/Filter.tsx";
+import {fetchSingleTodo} from "../../store/singletodo/singleTodoSlice.ts";
+import PopUp from "../../components/ui/popUp/PopUp.tsx";
 
 const HomePage = () => {
+    const navigate=useNavigate()
+    const isAuthorized=useAppSelector(state => state.auth.value)
+    const todo=useAppSelector(state => state.singleTodo.todo)
+    const [isFilterShown,setIsFilterShown]=useState(false)
+    const dispatch=useAppDispatch()
+
+    useEffect(()=>{
+        if (isAuthorized===false){
+            navigate("/login")
+        }
+    },[isAuthorized])
+    const handleRequest=()=>{
+       // getTodo()
+    }
+    // Init
+    useEffect(()=>{
+        dispatch(fetchSingleTodo())
+    },[!todo])
     return (
         <div className={classes.page__content}>
-            
+            <div>
+                <Button onClick={()=>setIsFilterShown(true)}>Фильтр</Button>
+                <Button onClick={handleRequest}>Запросить туду</Button>
+            </div>
+            {isFilterShown && <PopUp offerFunc={setIsFilterShown}>
+                <Filter/>
+            </PopUp>}
+            {todo && <Todo todo={todo}/>}
         </div>
     );
 };

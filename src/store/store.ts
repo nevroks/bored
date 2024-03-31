bb085b9e65479c78d6930532e3cc45631ca39720
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-31 23:26:42 +0500

    added logic of redux persist from documentation

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,42 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit'
-import todoReducer from "./todo/todoSlice.ts"
+import todoReducer from "./todos/todoSlice.ts"
 import authReducer from "./auth/authSlice.ts"
+import singleTodoReducer from "./singletodo/singleTodoSlice.ts"
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 const reducers = combineReducers(
     {
+        singleTodo:singleTodoReducer,
         todo:todoReducer,
         auth:authReducer
     })
-export const store = configureStore({
-    reducer: reducers,
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+const persistedReducers = persistReducer(persistConfig, reducers)
+
+
+const store= configureStore({
+    reducer: persistedReducers,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
+export const persistor=persistStore(store)
+export default store;
+
+
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch

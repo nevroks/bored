c5dd14103529f3a9abae0e5be17825c5a939130e
Nikita Babitsyn <nitubro1617@gmail.com>
2024-03-31 23:26:08 +0500

    wrapped app witch persist gate for redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import {Provider} from "react-redux";
-import {store} from "./store/store.ts";
+import store, {persistor} from "./store/store.ts";
 import {BrowserRouter} from "react-router-dom";
+import {PersistGate} from "redux-persist/integration/react";
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <Provider store={store}>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
-            <App />
-        </BrowserRouter>
-    </Provider>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <Provider store={store}>
+            <PersistGate loading={<h2>Синхронизация...</h2>} persistor={persistor}>
+                <App />
+            </PersistGate>
+        </Provider>
+    </BrowserRouter>
 )

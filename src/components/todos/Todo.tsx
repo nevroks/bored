f8437fcbb2eddff31e778dbff6208c7bb68c5404
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-03 16:13:21 +0500

    used ranges instead of hardcode values

--- src/components/todos/Todo.tsx
+++ src/components/todos/Todo.tsx
@@ -4,16 +4,16 @@ import {ITodo} from "../../types/types.tsx";
 type todoProps={
     todo:ITodo
 }
-
+import {PriceRange} from "../../consts/priceRange.ts";
+import {AcessibilityRange} from "../../consts/acessibilityRange.ts";
 const Todo: FC<todoProps> = ({todo}) => {
-    console.log(todo)
     return (
         <div>
             <p>Что делать:{todo.activity}</p>
-            <p>Доступность:{todo.accessibility === 0 ? "Максимальная" : todo.accessibility < 0.4 ? "Большая" : todo.accessibility < 0.7 ? "Средняя" : "Малая"}</p>
+            <p>Доступность:{todo.accessibility === AcessibilityRange.max.maxValue  ? "Максимальная" : todo.accessibility < AcessibilityRange.big.maxValue ? "Большая" : todo.accessibility < AcessibilityRange.mid.maxValue ? "Средняя" : "Малая"}</p>
             <p>Тип:{todo.type}</p>
 
-            <p>Цена:{todo.price === 0 ? "Бесплатно" : todo.price < 0.4 ? "Недорого" : todo.price < 0.7 ? "Средне" : "Дорого"}</p>
+            <p>Цена:{todo.price === PriceRange.free.maxValue ? "Бесплатно" : todo.price < PriceRange.almostFree.maxValue ? "Недорого" : todo.price < PriceRange.mid.maxValue ? "Средне" : "Дорого"}</p>
             <p>Кол-во людей:{todo.participants}</p>
         </div>
     );

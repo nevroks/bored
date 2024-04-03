6afaa39ae59a78b9331d05742c51bf47c727b8d1
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-03 16:10:09 +0500

    added async thunk for fn colling default action bc fn redux toolkit doesnt want to use it

--- src/store/singletodo/singleTodoSlice.ts
+++ src/store/singletodo/singleTodoSlice.ts
@@ -18,13 +18,27 @@ export const fetchSingleTodo=createAsyncThunk(
 
     }
 )
+export const setSingleTodo=createAsyncThunk(
+    //Бесполезная хуета я и атк понима. но у редакса ошибка вылезает
+    //с которой я ебался нахуй 2 дня и весь редакс перерыл
+    "setSingleTodo/fetchSingleTodo",
+    function (todo:ITodo,{dispatch}){
+        dispatch(setTodo(todo))
+    }
+)
 interface initState {
-    todo:ITodo | {},
+    todo:ITodo,
     status:string | null,
     error:string | null
 }
 const initialState:initState = {
-    todo:{},
+    todo:{
+        activity:'',
+        accessibility:0,
+        type:"",
+        participants: 0,
+        price:0,
+        key:0},
     status:null,
     error:null
 }
@@ -33,23 +47,24 @@ export const singleTodoSlice = createSlice({
     name: 'singleTodo',
     initialState,
     reducers: {
-        setTodo:(state,{payload})=>{
+        setTodo:(state,{payload}:PayloadAction<ITodo>)=>{
             state.todo=payload
         }
     },
     extraReducers:(builder)=>{
-        builder.addCase(fetchSingleTodo.pending, (state) => {
+        builder
+            .addCase(fetchSingleTodo.pending, (state) => {
             state.status = "loading";
             state.error=null
-        });
-        builder.addCase(fetchSingleTodo.fulfilled, (state,{payload}:PayloadAction<ITodo>) => {
+        })
+            .addCase(fetchSingleTodo.fulfilled, (state,{payload}:PayloadAction<ITodo>) => {
             state.status="resolved"
             state.todo=payload
-        });
-        builder.addCase(fetchSingleTodo.rejected, (state,{payload}:PayloadAction<string>) => {
+        })
+            .addCase(fetchSingleTodo.rejected, (state,{payload}:PayloadAction<string>) => {
             state.status="rejected"
             state.error=payload
-        });
+        })
     }
 })
 

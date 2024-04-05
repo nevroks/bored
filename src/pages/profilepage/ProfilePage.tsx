6d34d51c0b4ed3346d3d91ad4ff0ed487bff77db
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-05 17:39:07 +0500

    started working on profile page

--- src/pages/profilepage/ProfilePage.tsx
+++ src/pages/profilepage/ProfilePage.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
 import classes from "./style.module.css";
+import Button from "../../components/ui/button/Button.tsx";
+import {useAppDispatch, useAppSelector} from "../../hooks/reduxHooks.tsx";
+import {setAuth} from "../../store/auth/authSlice.ts";
+import TodosList from "../../components/todos/TodosList.tsx";
 
 const ProfilePage = () => {
+    const todos=useAppSelector(state => state.todo)
     return (
         <div className={classes.page__content}>
-            
+            <div>Настройки для профиля</div>
+            <div>
+                Список избранных:
+                <TodosList todos={todos}/>
+            </div>
         </div>
     );
 };

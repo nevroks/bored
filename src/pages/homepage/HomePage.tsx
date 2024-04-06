e4724e67284791a4e7ef9468c3efe13163b5d97c
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-06 21:25:08 +0500

    added lazy loading and now you cant get new todo before last isnt loaded

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -3,8 +3,6 @@ import classes from "./style.module.css";
 import {json, useNavigate} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "../../hooks/reduxHooks.tsx";
 import Button from "../../components/ui/button/Button.tsx";
-import {setAuth} from "../../store/auth/authSlice.ts";
-import UseGetData from "../../hooks/useGetData.ts";
 import Todo from "../../components/todos/Todo.tsx";
 import Filter from "../../components/filter/Filter.tsx";
 import {fetchSingleTodo} from "../../store/singletodo/singleTodoSlice.ts";
@@ -13,7 +11,7 @@ import PopUp from "../../components/ui/popUp/PopUp.tsx";
 const HomePage = () => {
     const navigate=useNavigate()
     const isAuthorized=useAppSelector(state => state.auth.value)
-    const todo=useAppSelector(state => state.singleTodo.todo)
+    const todo=useAppSelector(state => state.singleTodo)
     const filterSettings=useAppSelector(state => state.filterSettings)
     const [isFilterShown,setIsFilterShown]=useState(false)
     const [isFiltered,setIsFiltered]=useState(false)
@@ -33,14 +31,18 @@ const HomePage = () => {
     },[])
     return (
         <div className={classes.page__content}>
-            <div>
+            <div className={classes.navigation}>
                 <Button onClick={()=>setIsFilterShown(true)}>Фильтр</Button>
-                <Button onClick={handleRequest}>Запросить туду</Button>
+                <Button disabled={todo.status==="loading"} onClick={handleRequest}>Запросить туду{isFiltered && "(модиф)"}</Button>
             </div>
             {isFilterShown && <PopUp offerFunc={setIsFilterShown}>
                 <Filter setIsFiltered={setIsFiltered}/>
             </PopUp>}
-            {todo && <Todo todo={todo}/>}
+            <div className={classes.todo}>
+                <h2>Текущая задача:</h2>
+                {todo.status==="loading" ? <h2>Loading...</h2> : <Todo todo={todo.todo}/>}
+            </div>
+
         </div>
     );
 };

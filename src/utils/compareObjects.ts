6d4d64417cd3acfad757bc4e9e6df6def23f0130
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-06 21:19:11 +0500

    changed types in compareObjects function

--- src/utils/compareObjects.ts
+++ src/utils/compareObjects.ts
@@ -1,8 +1,4 @@
-type FNProps={
-    any:any
-}
-
-export default function CompareObjects (first:FNProps,second:FNProps){
+export default function CompareObjects (first:any,second:any){
     // @ts-ignore
     return Object.keys({ ...first, ...second }).every(key => first[key] === second[key]);
 }

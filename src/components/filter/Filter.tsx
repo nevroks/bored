66cd356baf7f8100dd59b8d8de7233fad67bb487
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-06 21:20:45 +0500

    added better typing and extended count of options

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useMemo, useRef, useState} from 'react';
+import {Dispatch, FC, SetStateAction, useEffect, useState} from 'react';
 import Button from "../ui/button/Button.tsx";
 import Input from "../ui/input/Input.tsx";
 import classes from "./style.module.css";
@@ -6,13 +6,15 @@ import {useAppDispatch, useAppSelector} from "../../hooks/reduxHooks.tsx";
 import CompareObjects from "../../utils/compareObjects.ts";
 import {PriceRange} from "../../consts/priceRange.ts";
 import {AcessibilityRange} from "../../consts/acessibilityRange.ts";
-import {fetchSingleTodo, setSingleTodo, setTodo} from "../../store/singletodo/singleTodoSlice.ts";
-import {setFilterSettings} from "../../store/filterSettings/filterSettingsSlice.ts";
+import {IFilerSettingsState, setFilterSettings} from "../../store/filterSettings/filterSettingsSlice.ts";
 
-const Filter = ({setIsFiltered}) => {
+type filterFCProps={
+    setIsFiltered:Dispatch<SetStateAction<boolean>>
+}
+
+const Filter:FC<filterFCProps> = ({setIsFiltered}) => {
     const dispatch=useAppDispatch()
     const filterSettings =useAppSelector(state => state.filterSettings)
-
     // ------------------------------------------
     //Управляемые инпуты и поля фильтра
 
@@ -26,14 +28,13 @@ const Filter = ({setIsFiltered}) => {
     )
     //Управляемые инпуты и поля фильтра
     // ------------------------------------------
-
+    const emptySettings:IFilerSettingsState={
+        accessibility:'',
+        type:"",
+        participants: '',
+        price:''
+    }
     useEffect(()=>{
-        const emptySettings={
-            accessibility:'',
-            type:"",
-            participants: '',
-            price:''
-        }
         if (CompareObjects(emptySettings,filterSettings)){
             setIsFiltered(false)
         }else{
@@ -44,6 +45,10 @@ const Filter = ({setIsFiltered}) => {
     function handleApply(){
         dispatch(setFilterSettings(inputsValue))
     }
+    function handleCancel(){
+        dispatch(setFilterSettings(emptySettings))
+        setInputsValue(emptySettings)
+    }
     return (
         <div className={classes.filter}>
             <label htmlFor="accessibility">Сортировка по доступности:
@@ -53,6 +58,9 @@ const Filter = ({setIsFiltered}) => {
                     defaultValue={''} name="accessibility" id="accessibility">
                     <option value="">Не узакано</option>
                     <option value={`${AcessibilityRange.max.maxValue}-${AcessibilityRange.max.minValue}`}>Максимальная</option>
+                    <option value={`${AcessibilityRange.big.maxValue}-${AcessibilityRange.big.minValue}`}>Большая</option>
+                    <option value={`${AcessibilityRange.mid.maxValue}-${AcessibilityRange.mid.minValue}`}>Средняя</option>
+                    <option value={`${AcessibilityRange.little.maxValue}-${AcessibilityRange.little.minValue}`}>Маленькая</option>
                 </select>
             </label>
             <label htmlFor="type">Сортировка по типу:
@@ -61,6 +69,15 @@ const Filter = ({setIsFiltered}) => {
                     value={inputsValue.type}
                     defaultValue={''} name="type" id="type">
                     <option value="">Не узакано</option>
+                    <option value="cooking">Готовка</option>
+                    <option value="education">Образование</option>
+                    <option value="recreational">Развлечения</option>
+                    <option value="social">Социальное</option>
+                    <option value="diy">Рукоблудие</option>
+                    <option value="charity">Благотворительное</option>
+                    <option value="relaxation">Отдых</option>
+                    <option value="music">Музыка</option>
+                    <option value="busywork">Рутина</option>
                 </select>
             </label>
             <label htmlFor="price">Сортировка по цене:
@@ -70,9 +87,12 @@ const Filter = ({setIsFiltered}) => {
                     defaultValue={''} name="price" id="price">
                     <option value="">Не узакано</option>
                     <option value={`${PriceRange.free.maxValue}-${PriceRange.free.minValue}`}>Бесплатно</option>
+                    <option value={`${PriceRange.almostFree.maxValue}-${PriceRange.almostFree.minValue}`}>Почти бесплатно</option>
+                    <option value={`${PriceRange.mid.maxValue}-${PriceRange.mid.minValue}`}>Средне</option>
+                    <option value={`${PriceRange.expensive.maxValue}-${PriceRange.expensive.minValue}`}>Дорого</option>
                 </select>
             </label>
-            <label htmlFor="price">Сортировка по количеству участников:
+            <label>Сортировка по количеству участников:
                 <Input
                     type={'number'}
                     onChange={e => setInputsValue({...inputsValue,participants:e.target.value})}
@@ -81,7 +101,7 @@ const Filter = ({setIsFiltered}) => {
             </label>
             <div className={classes.filter_btns}>
                 <Button onClick={handleApply}>Применить</Button>
-                <Button>Сбросить всё</Button>
+                <Button onClick={handleCancel}>Сбросить всё</Button>
             </div>
         </div>
     );

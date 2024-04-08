3e1b31dd17d1e31249ff24095c5055c447371c1b
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-08 12:37:29 +0500

    added logic of instruction poping and used instruction

--- src/pages/homepage/HomePage.tsx
+++ src/pages/homepage/HomePage.tsx
@@ -1,12 +1,13 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import {useEffect, useRef, useState} from 'react';
 import classes from "./style.module.css";
-import {json, useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "../../hooks/reduxHooks.tsx";
 import Button from "../../components/ui/button/Button.tsx";
 import Todo from "../../components/todos/Todo.tsx";
 import Filter from "../../components/filter/Filter.tsx";
 import {fetchSingleTodo} from "../../store/singletodo/singleTodoSlice.ts";
 import PopUp from "../../components/ui/popUp/PopUp.tsx";
+import Instruction from "../../components/instruction/Instruction.tsx";
 
 const HomePage = () => {
     const navigate=useNavigate()
@@ -15,6 +16,8 @@ const HomePage = () => {
     const filterSettings=useAppSelector(state => state.filterSettings)
     const [isFilterShown,setIsFilterShown]=useState(false)
     const [isFiltered,setIsFiltered]=useState(false)
+    const [isGuideUnChecked,setIsGuideUnChecked]=useState(true)
+    const countOfRenders=useRef(0)
     const dispatch=useAppDispatch()
 
     useEffect(()=>{
@@ -25,21 +28,47 @@ const HomePage = () => {
     const handleRequest=()=>{
         dispatch(fetchSingleTodo({isFiltered,filterSettings}))
     }
+    useEffect(()=>{
+        // @ts-ignore
+        if (JSON.parse(localStorage.getItem("checked"))!=null){
+            if (countOfRenders.current===0){
+                // @ts-ignore
+                setIsGuideUnChecked(JSON.parse(localStorage.getItem("checked")))
+            }
+            countOfRenders.current++
+        }
+
+        localStorage.setItem("checked",JSON.stringify(isGuideUnChecked))
+        if(!isGuideUnChecked){
+            document.body.style.overflow="auto"
+            document.body.style.paddingRight = "0"
+        }else{
+            document.body.style.overflow="hidden"
+            document.body.style.paddingRight = "15px"
+        }
+    },[isGuideUnChecked])
     // Init
     useEffect(()=>{
+
+        // @ts-ignore
         dispatch(fetchSingleTodo())
     },[])
+    // @ts-ignore
     return (
         <div className={classes.page__content}>
             <div className={classes.navigation}>
                 <Button onClick={()=>setIsFilterShown(true)}>Фильтр</Button>
                 <Button disabled={todo.status==="loading"} onClick={handleRequest}>Запросить туду{isFiltered && "(модиф)"}</Button>
             </div>
-            {isFilterShown && <PopUp offerFunc={setIsFilterShown}>
+            {isFilterShown && <PopUp type={"popUp"} offerFunc={setIsFilterShown}>
                 <Filter setIsFiltered={setIsFiltered}/>
+            </PopUp>}
+            {isGuideUnChecked && <PopUp type={"instruction"} offerFunc={setIsGuideUnChecked}>
+                <Instruction/>
             </PopUp>}
             <div className={classes.todo}>
                 <h2>Текущая задача:</h2>
+                {/*@ts-ignore*/}
                 {todo.status==="loading" ? <h2>Loading...</h2> : <Todo todo={todo.todo}/>}
             </div>
 

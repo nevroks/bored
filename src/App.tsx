c6b1e2e4642794775840d3b48e0a19789fb7a1fd
Nikita Babitsyn <nitubro1617@gmail.com>
2024-04-08 12:34:54 +0500

    deleted todos page and removed it from routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {Route, Routes} from "react-router-dom";
 import Layout from "./components/layout/Layout.tsx";
 import HomePage from "./pages/homepage/HomePage.tsx";
-import TodosPage from "./pages/todospage/TodosPage.tsx";
 import ProfilePage from "./pages/profilepage/ProfilePage.tsx";
 import LoginPage from "./pages/loginpage/LoginPage.tsx";
 
@@ -14,7 +13,6 @@ function App() {
         <Route path={"/login"} element={<LoginPage/>}/>
         <Route path="/" element={<Layout/>}>
           <Route index element={<HomePage/>}/>
-          <Route path="todos" element={<TodosPage/>}/>
           <Route path="profile" element={<ProfilePage/>}/>
         </Route>
       </Routes>
